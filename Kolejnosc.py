54f6bbf5915b5b6d4b73fa190007c8f24647db7c
kapiso44 <143086366+kapiso44@users.noreply.github.com>
2025-06-09 16:08:59 +0200

    Import full implementation from reference

--- Kolejnosc.py
+++
@@ -1,5 +0,0 @@
-def pobierz_kolejnosc_organizmow(self):
-    """Zwraca organizmy posortowane według kolejności działania"""
-    return sorted(self.organizmy,
-                 key=lambda org: (-org.inicjatywa, -org.wiek),
-                 reverse=False)
